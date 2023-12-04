5ac86597172e8d13a1c637eb70ae5fdf5ebe504e
Owan Ayuk Daisy Ajijia <105789241+OwanDaisy@users.noreply.github.com>
2023-12-04 19:35:37 +0100

    Add files via upload

---
+++ Music_Player/script.js
@@ -0,0 +1,40 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrl-icon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.ondurationchange;
+    progress.value = song.currentTime;
+}
+
+// creatin play pause button to work
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    }
+    else{
+        song.play();
+        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+    }
+}
+
+// makin slider to sync as song plays
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500)
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
+}
+
+
+
